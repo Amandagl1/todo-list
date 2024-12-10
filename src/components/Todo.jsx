7c83d40aedb2dfbb7687d632fe287f3176ea6f7d
Amanda Lee <leeandama87@gmail.com>
2024-12-10 14:41:39 -0800

    Added useState hook and function to log user inputted values to the console.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems.jsx'
 
 const Todo = () => {
+
+const [todoList, setTodoList] = useState([]);
+
+{/* Add functionality to the code. When user inputs a text and presses the add button,
+     the new task gets added to the list */}
+const inputRef = useRef();
+
+const addText = () => {
+    {/* Add .trim to remove spaces from beginning and end of text */}
+    const inputText = inputRef.current.value.trim();
+    {/* Log the inputted value to the console */}
+    console.log(inputText);
+}
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md
     flex flex-col p-7 min-h-[550px] rounded-xl'>
@@ -18,10 +32,10 @@ const Todo = () => {
 
     <div className='flex items-center my-7 bg-gray-200
     rounded-full'>
-        <input className='bg-transparent border-0 outline-none
+        <input ref={inputRef} className='bg-transparent border-0 outline-none
         flex-1 h-12 pl-6 pr-2 placeholder:text-slate-600' 
         type="text" placeholder='Add a task'/>
-        <button className='border-none rounded-full 
+        <button onClick={addText} className='border-none rounded-full 
         bg-orange-600 w-28 h-14 text-white text-lg
         font-medium cursor-pointer'>ADD +</button>
     </div>
@@ -29,7 +43,8 @@ const Todo = () => {
 {/* --To Do List-- */}
 
     <div>
-        <TodoItems />
+        <TodoItems text="Learn Coding"/>
+        <TodoItems text="Practice for job interviews"/>
     </div>
 
     </div>
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -6,10 +6,17 @@ import delete_icon from '../assets/delete.png'
 const TodoItems = () => {
   return (
     <div className='flex items-center my-3 gap-2'>
-      <div>
-        <img src={tick} alt="" />
-        <p>Learn Coding</p>
+
+      <div className='flex flex-1 items-center cursor-pointer'>
+        <img src={tick} alt=""className='w-7' />
+        <p className='text-slate-700 ml-4 text-[17px]'>
+            Learn Coding
+        </p>
       </div>
+
+      {/* Delete icon image */}
+      <img src={delete_icon} alt="" className='w-3.5 cursor-pointer' />
+
     </div>
   )
 }
