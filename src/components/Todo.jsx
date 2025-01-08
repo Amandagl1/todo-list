2689fcd2c47f7853ee2562576b57c70f8f1b82b7
Amanda Lee <leeandama87@gmail.com>
2025-01-08 14:59:30 -0800

    Added useRef from React.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,22 +1,54 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
-import TodoItems from './TodoItems.jsx'
+import TodoItems from './TodoItems'
 
 const Todo = () => {
+
+{/* Initialize the useState hook with [] (empty array)
+    where multiple values will be stored */}
 
 const [todoList, setTodoList] = useState([]);
 
 {/* Add functionality to the code. When user inputs a text and presses the add button,
      the new task gets added to the list */}
+      
 const inputRef = useRef();
 
-const addText = () => {
-    {/* Add .trim to remove spaces from beginning and end of text */}
-    const inputText = inputRef.current.value.trim();
-    {/* Log the inputted value to the console */}
-    console.log(inputText);
-}
+{/* ----- Add a task ----- */}
 
+const addTodo = () => {
+    // Add .trim to remove spaces from beginning and end of text
+    const inputText = inputRef.current.value.trim();
+
+    // Condition that checks if there is any input to prevent empty todos
+    if (inputText === "") {
+        return null;
+    }
+
+    const newTodo = {
+        id: Date.now(),
+        text: inputText,
+        isComplete: false,
+    }
+
+    // Update the todo list
+    setTodoList((prev) => [...prev, newTodo]);
+
+    // Clear input field
+    inputRef.current.value = "";
+
+    // Log the input text
+    console.log(inputText);
+};
+
+{/* ----- Delete a task ----- */}
+
+const deleteTodo = (id) => {
+    //Update state by filtering through todos and returning all but id
+    setTodoList((prevTodos) => prevTodos.filter((todo) => todo.id != id))
+};
+
+{/* ----- Display ----- */}
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md
     flex flex-col p-7 min-h-[550px] rounded-xl'>
@@ -30,24 +62,35 @@ const addText = () => {
 
     {/* --Input box-- */}
 
-    <div className='flex items-center my-7 bg-gray-200
-    rounded-full'>
+    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
         <input ref={inputRef} className='bg-transparent border-0 outline-none
         flex-1 h-12 pl-6 pr-2 placeholder:text-slate-600' 
         type="text" placeholder='Add a task'/>
-        <button onClick={addText} className='border-none rounded-full 
-        bg-orange-600 w-28 h-14 text-white text-lg
-        font-medium cursor-pointer'>ADD +</button>
+
+        <button type='button' onClick={addTodo} 
+        className='border-none rounded-full cursor-pointer
+        w-28 h-14 bg-orange-600 text-white text-lg font-medium'>
+            ADD +
+        </button>
     </div>
 
 {/* --To Do List-- */}
 
     <div>
+        {todoList.map((item) => {
+            return <TodoItems text={item.text} id={item.id}
+            isComplete={item.isComplete} />
+        })}
+
+    {/* Test to display sample text
+
         <TodoItems text="Learn Coding"/>
         <TodoItems text="Practice for job interviews"/>
-    </div>
+    */}
 
     </div>
+
+    </div> 
   )
 }
 
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -3,14 +3,16 @@ import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 
-const TodoItems = () => {
+// Add destructured items in props
+const TodoItems = ({text, id, isComplete}) => {
+
   return (
     <div className='flex items-center my-3 gap-2'>
 
       <div className='flex flex-1 items-center cursor-pointer'>
         <img src={tick} alt=""className='w-7' />
         <p className='text-slate-700 ml-4 text-[17px]'>
-            Learn Coding
+            {text}
         </p>
       </div>
 
@@ -19,6 +21,7 @@ const TodoItems = () => {
 
     </div>
   )
+  
 }
 
 export default TodoItems
